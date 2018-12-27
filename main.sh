756b7552fec048569a741154e43e5729be13b379
tjonestech <tjones.tech@gmail.com>
2018-12-27 12:05:59 -0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,24 @@
+
+# C02XHCNEJG5J:~ jonesth6$ brew install p7zip
+# Updating Homebrew...
+# ==> Downloading https://homebrew.bintray.com/bottles/p7zip-16.02_1.high_sierra.b
+######################################################################## 100.0%
+==> Pouring p7zip-16.02_1.high_sierra.bottle.tar.gz
+# 🍺  /usr/local/Cellar/p7zip/16.02_1: 103 files, 4.7MB
+# C02XHCNEJG5J:~ jonesth6$ which 7za
+
+
+/usr/local/bin/7za
+
+
+# C02XHCNEJG5J:~ jonesth6$ 7za
+
+# 7-Zip (a) [64] 16.02 : Copyright (c) 1999-2016 Igor Pavlov : 2016-05-21
+# p7zip Version 16.02 (locale=utf8,Utf16=on,HugeFiles=on,64 bits,12 CPUs x64)
+
+
+/usr/bin/gunzip -c foo.tar.gz | /usr/bin/tar xopf -
+
+/sbin/md5
+
+/usr/bin/openssl
